77315ea90edef2c0be11019b9ea4c41a08c4f584
bright_spark <markpassey@btinternet.com>
2017-04-13 12:57:33 +0100

    Updated to version 1.10.2-0.1.0

--- src/main/java/brightspark/stem/STEM.java
+++ src/main/java/brightspark/stem/STEM.java
@@ -28,7 +28,7 @@ public class STEM
 {
     public static final String MOD_ID = "stem";
     public static final String MOD_NAME = "S.T.E.M";
-    public static final String VERSION = "1.10.2-0.0.8";
+    public static final String VERSION = "1.10.2-0.1.0";
     public static final String GUI_TEXTURE_DIR = "textures/gui/";
     public static File CONFIG_DIR;
 
